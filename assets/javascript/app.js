b7eec8201efd0bb1c089786dba939836e3a8c52e
MarkLiebs <marklieberman@Marks-MacBook-Pro-2.local>
2016-05-15 23:40:24 -0400

    Continuing JS coding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,3 +39,36 @@ $("#trainSubmit").on("click", function(){
 
     return false;
 });
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+trainData.on("child_added", function(childSnapshot, prevChildKey){
+
+	console.log(childSnapshot.val());
+
+	// Store everything into a variable.
+	var Name = childSnapshot.val().name;
+	var Dest = childSnapshot.val().dest;
+	var Time = childSnapshot.val().time;
+	var Freq = childSnapshot.val().freq;
+
+	// Employee Info
+	console.log(Name);
+	console.log(Dest);
+	console.log(Time);
+	console.log(Freq);
+
+	// // Prettify the employee start
+	// var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+	// // Calculate the months worked using hardconre math
+	// // To calculate the months worked 
+	// var empMonths = moment().diff(moment.unix(empStart, 'X'), "months");
+	// console.log(empMonths);
+
+	// // Calculate the total billed rate
+	// var empBilled = empMonths * empRate;
+	// console.log(empBilled);
+
+	// // Add each train's data into the table 
+	// $("#employeeTable > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" + empStartPretty + "</td><td>" + empRate + "</td><td>" + empMonths + "</td><td>" + empBilled + "</td></tr>");
+
+});
